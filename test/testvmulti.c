3711718959474f11d06d1bf0dbc245fc8647fca1
Daniel Newton <djpnewton@gmail.com>
2011-04-30 12:53:53 +1200

    * changed maximum multi-touch count to 20 (using a hybrid report) and updated client library

---
+++ test/testvmulti.c
@@ -0,0 +1,206 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vmulticlient.h"
+
+//
+// Function prototypes
+//
+
+VOID
+SendHidRequests(
+    pvmulti_client vmulti,
+    BYTE requestType
+    );
+
+//
+// Implementation
+//
+
+void
+Usage(void)
+{
+    printf("Usage: testvmulti </multitouch | /mouse | /digitizer | /joystick>\n");
+}
+
+INT __cdecl
+main(
+    int argc,
+    PCHAR argv[]
+    )
+{
+    BYTE   reportId;
+    pvmulti_client vmulti;
+
+    UNREFERENCED_PARAMETER(argv);
+
+    //
+    // Parse command line
+    //
+
+    if (argc == 1)
+    {
+        Usage();
+        return 1;
+    }
+    if (strcmp(argv[1], "/multitouch") == 0)
+    {
+        reportId = REPORTID_MTOUCH;
+    }
+    else if (strcmp(argv[1], "/mouse") == 0)
+    {
+        reportId = REPORTID_MOUSE;
+    }
+    else if (strcmp(argv[1], "/digitizer") == 0)
+    {
+        reportId = REPORTID_DIGI;
+    }
+    else if (strcmp(argv[1], "/joystick") == 0)
+    {
+        reportId = REPORTID_JOYSTICK;
+    }
+    else
+    {
+        Usage();
+        return 1;
+    }
+
+    //
+    // File device
+    //
+
+    vmulti = vmulti_alloc();
+
+    if (vmulti == NULL)
+    {
+        return 2;
+    }
+
+    if (!vmulti_connect(vmulti))
+    {
+        vmulti_free(vmulti);
+        return 3;
+    }
+
+    printf("...sending write request to our device\n");
+    SendHidRequests(vmulti, reportId);
+
+    vmulti_disconnect(vmulti);
+    vmulti_free(vmulti);
+
+    return 0;
+}
+
+VOID
+SendHidRequests(
+    pvmulti_client vmulti,
+    BYTE requestType
+    )
+{
+    switch (requestType)
+    {
+        case REPORTID_MTOUCH:
+        {
+            //
+            // Send the multitouch reports
+            //
+            
+            BYTE i;
+            BYTE actualCount = 4; // set whatever number you want, lower than MULTI_MAX_COUNT
+            PTOUCH pTouch = (PTOUCH)malloc(actualCount * sizeof(TOUCH));
+
+            printf("Sending multitouch report\n");
+
+            for (i = 0; i < actualCount; i++)
+            {
+                pTouch[i].ContactID = i;
+                pTouch[i].Status = MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT | MULTI_TIPSWITCH_BIT;
+                pTouch[i].XValue = (i + 1) * 1000;
+                pTouch[i].YValue = (i + 1) * 1500 + 5000;
+                pTouch[i].Width = 20;
+                pTouch[i].Height = 30;
+            }
+
+            if (!vmulti_update_multitouch(vmulti, pTouch, actualCount))
+              printf("vmulti_update_multitouch TOUCH_DOWN FAILED\n");
+              
+            for (i = 0; i < actualCount; i++)
+            {
+                pTouch[i].XValue += 1000;
+                pTouch[i].YValue += 1000;
+            }              
+
+            if (!vmulti_update_multitouch(vmulti, pTouch, actualCount))
+                printf("vmulti_update_multitouch TOUCH_MOVE FAILED\n");
+
+            for (i = 0; i < actualCount; i++)
+              pTouch[i].Status = 0;
+
+            if (!vmulti_update_multitouch(vmulti, pTouch, actualCount))
+                printf("vmulti_update_multitouch TOUCH_UP FAILED\n");
+                        
+            free(pTouch);
+            
+            break;
+        }
+
+        case REPORTID_MOUSE:
+            //
+            // Send the mouse report
+            //
+            printf("Sending mouse report\n");
+            vmulti_update_mouse(vmulti, 0, 1000, 10000, 0);
+            break;
+
+        case REPORTID_DIGI:
+            //
+            // Send the digitizer reports
+            //
+            printf("Sending digitizer report\n");
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 10000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 12000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 14000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 16000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 18000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 2000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 3000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 15000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 10000);
+            vmulti_update_digi(vmulti, 0, 3000, 10000);
+            break;
+
+        case REPORTID_JOYSTICK:
+        {
+            //
+            // Send the joystick report
+            //
+            BYTE buttons = 0, x = 0, y = 128, throttle = 0;
+            printf("Sending joystick report\n");
+            while (1)
+            {
+                vmulti_update_joystick(vmulti, buttons, x, y, throttle);
+                buttons++;
+                x++;
+                y++;
+                throttle++;
+                Sleep(10);
+            }
+            break;
+        }
+    }
+}
+
+
